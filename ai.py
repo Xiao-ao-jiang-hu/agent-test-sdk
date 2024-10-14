d40413bf3724872a481e9125a540caf31e397268
校奥浆糊 <wst22@mails.tsinghua.edu.cn>
2024-10-14 22:17:05 +0800

    feat: ai

--- ai.py
+++ ai.py
@@ -15,19 +15,4 @@ def ai(env: EliminationEnv):
     Returns:
         int: 操作对应的序号，可以使用env的coord_to_num方法将坐标转换为操作序号
     """
-    max_reward = 0
-    max_action = 0
-    start = time.time()
-    actions = list(range(160000))
-    random.shuffle(actions)
-    for i in actions:
-        env_copy = deepcopy(env)
-        board, reward, end = env_copy.step(i)
-        if reward > max_reward:
-            max_reward = reward
-            max_action = i
-
-        if time.time() - start > 0.9:
-            break
-    print(f"searched {i+1} operations", file=sys.stderr)
-    return max_action
+    return 0
